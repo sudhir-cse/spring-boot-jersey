1a46e872eadc4de9c0c988c9e8d2ed048bad5544
sudhir <sudhir.cse.09@gmail.com>
2017-05-30 10:00:39 +0200

    Added CORSResponseFilter

--- src/main/java/ag/it/norcom/config/JerseyConfig.java
+++ src/main/java/ag/it/norcom/config/JerseyConfig.java
@@ -4,11 +4,13 @@ import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.stereotype.Component;
 
 import ag.it.norcom.endpoint.EmailResource;
+import ag.it.norcom.filters.CORSResponseFilter;
 
 @Component
 public class JerseyConfig extends ResourceConfig {
 
 	public JerseyConfig() {
 		register(EmailResource.class);
+		register(CORSResponseFilter.class);
 	}
 }
